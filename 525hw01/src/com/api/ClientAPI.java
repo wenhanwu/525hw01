d6cbdcd432ec3a072cf979857478bc76fb5349b8
Haijun Qiao <haijunq@gmail.com>
2013-05-27 16:23:22 -0700

    get start!

---
+++ 525hw01/src/com/api/ClientAPI.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.api;
+
+/**
+ *
+ * @author haijun
+ */
+public class ClientAPI {
+    
+}
