bdf6f4843639f207ad65bcf65b8bdd84c7e78191
Haijun Qiao <haijunq@gmail.com>
2013-05-27 17:32:06 -0700

    recover user class

--- 525hw01/src/com/server/User.java
+++ 525hw01/src/com/server/User.java
@@ -4,10 +4,41 @@
  */
 package com.server;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author w
  */
 public class User {
-    
+    private int userID;
+    private String userName;
+
+    /**
+     * @return the userID
+     */
+    public int getUserID() {
+        return userID;
+    }
+
+    /**
+     * @param userID the userID to set
+     */
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
